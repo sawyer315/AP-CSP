37fd5e3f7135c9b1d7bc61fde6e2b1218c5372d9
sawyer315 <143819017+sawyer315@users.noreply.github.com>
2023-09-07 14:18:37 +0000

    finished

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-Radius = 5
+Radius = 3.712
 Pi = 3.14159
-Area = Pi * Radius * Radius
-Cirsumference = 2 * Radius
+Area = Pi * (Radius * Radius)
+Circumference = 2 * Pi * Radius
 
-round(Area, 2)
+Area = round(Area, 3)
+Circumference = round(Circumference, 3)
+
+print(Radius)
 print(Area)
+print(Circumference)
