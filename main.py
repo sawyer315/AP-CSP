2f978084ad142f6f9bea48518312b4e29015cc37
sawyer315 <25993470-sawyer315@users.noreply.replit.com>
2023-09-07 14:24:48 +0000

    input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-Radius = 3.712
+print("Radius: ")
+Radius = float(input())
 Pi = 3.14159
 Area = Pi * (Radius * Radius)
 Circumference = 2 * Pi * Radius
@@ -6,6 +7,6 @@ Circumference = 2 * Pi * Radius
 Area = round(Area, 3)
 Circumference = round(Circumference, 3)
 
-print(Radius)
-print(Area)
-print(Circumference)
+print("Radius: ", Radius)
+print("Area: ", Area)
+print("Circumference: ", Circumference)
